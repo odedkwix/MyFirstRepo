ee1629f2027ec361a31eb2ae796e8a239035959a
Oded Kristianpoller <odedk@wix.com>
2021-04-22 09:18:01 +0300

    add picture

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -6,7 +6,7 @@ let myImg = document.querySelector('img');
 myImg.onclick = () => {
 	let mySrc = myImg.getAttribute('src');
 	if (MySrc === 'ihttps://www.humanesociety.org/sites/default/files/styles/2000x850/public/2020-07/kitten-510651.jpg?h=f54c7448&itok=lJefJMMQ'){
-		myImg.setAttribute('src','images/cat2')
+		myImg.setAttribute('src','https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop')
 	}
 	
 }
