3c9f49cf4c49a03d0beb53530d9286b3d2b0ddd8
Oded Kristianpoller <odedk@wix.com>
2021-04-22 09:21:44 +0300

    add script to change image

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -3,10 +3,10 @@ myHeading.textContent = 'Hello world!';
 
 let myImg = document.querySelector('img');
 
-myImg.onclick = () => {
+myImg.onclick = function () {
 	let mySrc = myImg.getAttribute('src');
 	if (MySrc === 'ihttps://www.humanesociety.org/sites/default/files/styles/2000x850/public/2020-07/kitten-510651.jpg?h=f54c7448&itok=lJefJMMQ'){
-		myImg.setAttribute('src','https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop')
+		myImg.setAttribute('src','https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop');
 	}
 	
 }
