eec7e6bf8cbbdc46f4c5ffc2ab086dcc3fec78b4
Oded Kristianpoller <odedk@wix.com>
2021-04-22 09:14:42 +0300

    add images

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,2 +1,12 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+let myImg = document.querySelector('img');
+
+myImg.onclick = () => {
+	let mySrc = myImg.getAttribute('src');
+	if (MySrc === 'ihttps://www.humanesociety.org/sites/default/files/styles/2000x850/public/2020-07/kitten-510651.jpg?h=f54c7448&itok=lJefJMMQ'){
+		myImg.setAttribute('src','images/cat2')
+	}
+	
+}
